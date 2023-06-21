e4bf71e1ec40412f31ee55f6d9728d4fc6dfb01f
kyoh86 <me@kyoh86.dev>
2023-06-22 00:12:03 +0900

    fix :Rename to suspend process if the path not changed; fix: #6

--- nvim/lua/kyoh86/conf/rename.lua
+++ nvim/lua/kyoh86/conf/rename.lua
@@ -2,6 +2,9 @@
 local function rename(e)
   local newname = e.fargs[1]
   local oldname = vim.api.nvim_buf_get_name(0)
+  if vim.uv.fs_realpath(newname) == vim.uv.fs_realpath(oldname) then
+    return
+  end
   vim.cmd("saveas" .. (e.bang and "! " or " ") .. "++p" .. newname)
   vim.fn.delete(oldname)
   vim.cmd("silent! edit")
