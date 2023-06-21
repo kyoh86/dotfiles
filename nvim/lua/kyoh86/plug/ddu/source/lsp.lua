cb00d9ed0054d3eb47416772b4aef1afe6e7bc67
kyoh86 <me@kyoh86.dev>
2023-06-22 00:12:04 +0900

    ddu-source-lsp-codeaction

--- nvim/lua/kyoh86/plug/ddu/source/lsp.lua
+++ nvim/lua/kyoh86/plug/ddu/source/lsp.lua
@@ -4,6 +4,15 @@ local helper = require("kyoh86.plug.ddu.helper")
 local spec = {
   "uga-rosa/ddu-source-lsp",
   config = function()
+    vim.fa.ddu.custom.patch_global({ kindOptions = {
+      lsp = {
+        defaultAction = "open",
+      },
+      lsp_codeAction = {
+        defaultAction = "apply",
+      },
+    } })
+
     helper.start_by("<leader>fld", "lsp_definition", {
       sources = { { name = "lsp_definition" } },
     })
@@ -16,11 +25,7 @@ local spec = {
 
     helper.start_by("<leader>flw", "lsp_workspaceSymbol", {
       sources = { { name = "lsp_workspaceSymbol" } },
-      sourceOptions = {
-        lsp = {
-          volatile = true,
-        },
-      },
+      sourceOptions = { lsp = { volatile = true } },
     })
     helper.map_for_file("lsp_workspaceSymbol")
 
@@ -31,11 +36,7 @@ local spec = {
           method = "callHierarchy/outgoingCalls",
         },
       } },
-      sourceOptions = {
-        lsp = {
-          volatile = true,
-        },
-      },
+      sourceOptions = { lsp = { volatile = true } },
       uiParams = {
         ff = {
           displayTree = true,
@@ -44,6 +45,11 @@ local spec = {
       },
     })
     helper.map_for_file("lsp_callHierarchy")
+
+    vim.keymap.set({ "n", "x" }, "<leader>fla", "<cmd>call ddu#start({'sources': [{'name': 'lsp_codeAction'}]})<cr>", {
+      remap = false,
+      desc = "Start ddu source: lsp_codeAction",
+    })
   end,
   dependencies = { { "Shougo/ddu.vim" } },
 }
