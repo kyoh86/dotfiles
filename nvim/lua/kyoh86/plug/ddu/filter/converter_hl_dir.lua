b23e9db4999e98eaff18e4bc95b0e1e73dd6cfbe
kyoh86 <me@kyoh86.dev>
2023-06-25 01:48:06 +0900

    use zebra color for ddu-files

--- nvim/lua/kyoh86/plug/ddu/filter/converter_hl_dir.lua
+++ nvim/lua/kyoh86/plug/ddu/filter/converter_hl_dir.lua
@@ -3,8 +3,18 @@ local helper = require("kyoh86.plug.ddu.helper")
 ---@type LazySpec
 local spec = {
   "kyoh86/ddu-filter-converter_hl_dir",
-  dev = true,
   config = function()
+    ensure("momiji", function(m)
+      vim.api.nvim_set_hl(0, "dduDir1", { fg = m.colors.green })
+      vim.api.nvim_set_hl(0, "dduDir2", { fg = m.colors.lightgreen })
+      vim.fa.ddu.custom.patch_global({
+        filterParams = {
+          converter_hl_dir = {
+            hlGroup = { "dduDir1", "dduDir2" },
+          },
+        },
+      })
+    end)
     vim.fa.ddu.custom.patch_global({
       sourceOptions = {
         _ = {
