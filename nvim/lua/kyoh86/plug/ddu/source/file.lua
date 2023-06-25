b2de4410fc59ca262d835a9e476201c0efda7795
kyoh86 <me@kyoh86.dev>
2023-06-25 15:00:02 +0900

    ddu-source-fileの取り扱いを整理した
    fix: #10

--- nvim/lua/kyoh86/plug/ddu/source/file.lua
+++ nvim/lua/kyoh86/plug/ddu/source/file.lua
@@ -1,40 +1,22 @@
 local helper = require("kyoh86.plug.ddu.helper")
 
-local function files_or_gitls()
-  local stat = vim.uv.fs_stat(".git")
-  if stat and stat.type == "directory" then
-    vim.fa.ddu.start({
-      name = "file-ext",
-      sources = { {
-        name = "file_external",
-        params = { cmd = { "git", "ls-files" } },
-      } },
-    })
-  else
-    vim.fa.ddu.start({
-      name = "file-rec",
-      sources = { {
-        name = "file_rec",
-      } },
-    })
-  end
-end
-
 ---@type LazySpec
 local spec = {
   "matsui54/ddu-source-file_external",
   config = function()
-    -- setup source for files or gitls
-    vim.keymap.set("n", "<leader>ff", files_or_gitls, { remap = false, desc = "Start ddu to list files or git-ls files" })
-    helper.start_by("<leader>fgf", "file-ext", {
+    helper.start_by("<leader>ff", "file-hide", {
       sources = { {
         name = "file_external",
-        params = { cmd = { "git", "ls-files" } },
+        params = { cmd = { "rg", "--files", "--color", "never" } },
       } },
     })
 
-    helper.map_for_file("file-rec")
-    helper.map_for_file("file-ext")
+    helper.start_by("<leader>faf", "file-all", {
+      sources = { { name = "file_rec" } },
+    })
+
+    helper.map_for_file("file-hide")
+    helper.map_for_file("file-all")
 
     -- setup source for nvim-configs
     helper.start_by("<leader><leader>c", "nvim-config", { sources = { { name = "file_rec", options = { path = vim.env.XDG_CONFIG_HOME } } } })
