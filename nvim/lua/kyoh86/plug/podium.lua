303fb9ab737f4edf113c91ecf42653ea700bda3c
kyoh86 <me@kyoh86.dev>
2023-07-07 00:45:35 +0900

    refactor podium setting

--- nvim/lua/kyoh86/plug/podium.lua
+++ nvim/lua/kyoh86/plug/podium.lua
@@ -1,54 +1,47 @@
----@param buffer integer
----@param outfile string a output file
----@param filetype "html"|"markdown"|"vimdoc"
-local function processToFile(buffer, outfile, filetype)
-  local podium = require("podium")
-  local processor = podium.PodiumProcessor.new(podium[filetype])
-  local content = vim.api.nvim_buf_get_lines(buffer, 0, -1, true)
-  local processed = processor:process(table.concat(content, "\n"))
-  local mode = 420 -- 0644(8) == 420(10)
-  local fp = vim.uv.fs_open(outfile, "w", mode)
-  vim.uv.fs_write(fp, processed)
-  vim.uv.fs_close(fp)
-  --TODO: reload buffers loading the outname
-end
-
----@param filetype "html"|"markdown"|"vimdoc"|nil
----@return "html"|"markdown"|"vimdoc"
-local function defaultFiletype(filetype)
-  if not filetype or filetype == "" then
-    return "vimdoc"
-  end
-  return filetype
-end
-
 local exts = {
   html = ".html",
   markdown = ".md",
   vimdoc = ".txt",
 }
 
----@param podname string
+---@param buffer integer
+---@param filetype "html"|"markdown"|"vimdoc"|nil
+---@return "html"|"markdown"|"vimdoc",string
+local function preprocess(buffer, filetype)
+  if not filetype or filetype == "" then
+    filetype = "vimdoc"
+  end
+  local bufname = vim.api.nvim_buf_get_name(buffer)
+  local filename = string.gsub(bufname, "%.pod$", exts[filetype])
+  return filetype, filename
+end
+
+---@param buffer integer
 ---@param filetype "html"|"markdown"|"vimdoc"
----@return string
-local function convertFilename(podname, filetype)
-  local outname = string.gsub(podname, "%.pod$", exts[filetype])
-  return outname
+---@param filename string a output file
+local function processToFile(buffer, filetype, filename)
+  local podium = require("podium")
+  local processor = podium.PodiumProcessor.new(podium[filetype])
+  local content = vim.api.nvim_buf_get_lines(buffer, 0, -1, true)
+  local processed = processor:process(table.concat(content, "\n"))
+  local mode = 420 -- 0644(8) == 420(10)
+  local fp = vim.uv.fs_open(filename, "w", mode)
+  vim.uv.fs_write(fp, processed)
+  vim.uv.fs_close(fp)
+  --TODO: reload buffers loading the outname
 end
 
 ---Make preview window for the podium
 ---@param buffer integer
----@param filetype "html"|"markdown"|"vimdoc"|nil
+---@param filetype "html"|"markdown"|"vimdoc"
+---@param filename string
 ---@param preview boolean
-local function processToBuffer(buffer, filetype, preview)
+local function processToBuffer(buffer, filetype, filename, preview)
   local podium = require("podium")
-  local ft = defaultFiletype(filetype)
-  local input = table.concat(vim.api.nvim_buf_get_lines(buffer, 0, -1, true), "\n")
-  local processor = podium.PodiumProcessor.new(podium[ft])
-  local output = processor:process(input)
-  local bufname = vim.api.nvim_buf_get_name(buffer)
-  local outname = convertFilename(bufname, ft)
-  vim.cmd.new(outname)
+  local content = table.concat(vim.api.nvim_buf_get_lines(buffer, 0, -1, true), "\n")
+  local processor = podium.PodiumProcessor.new(podium[filetype])
+  local output = processor:process(content)
+  vim.cmd.new(filename)
   vim.api.nvim_buf_set_lines(0, 0, -1, true, vim.split(output, "\n"))
   if preview then
     vim.bo[0].modified = false
@@ -66,20 +59,18 @@ local spec = {
       pattern = "pod",
       callback = function(ev)
         vim.api.nvim_buf_create_user_command(ev.buf, "PodiumWrite", function(opts)
-          local ft = defaultFiletype(opts.args)
-          local bufname = vim.api.nvim_buf_get_name(ev.buf)
-          local outname = convertFilename(bufname, ft)
-          if vim.fn.filereadable(outname) ~= 0 then
+          local filetype, filename = preprocess(ev.buf, opts.args)
+          if vim.fn.filereadable(filename) ~= 0 then
             vim.ui.select({ "yes", "no" }, {
-              prompt = "There's already " .. outname .. "; Are you sure you overwrite it?: ",
+              prompt = "There's already " .. filename .. "; Are you sure you overwrite it?: ",
             }, function(choice)
               if choice ~= "yes" then
                 return
               end
-              processToFile(ev.buf, outname, ft)
+              processToFile(ev.buf, filetype, filename)
             end)
           else
-            processToFile(ev.buf, outname, ft)
+            processToFile(ev.buf, filetype, filename)
           end
         end, {
           desc = "Convert pod with podium and write it to file",
@@ -87,14 +78,16 @@ local spec = {
           force = true,
         })
         vim.api.nvim_buf_create_user_command(ev.buf, "Podium", function(opts)
-          processToBuffer(ev.buf, opts.args, false)
+          local filetype, filename = preprocess(ev.buf, opts.args)
+          processToBuffer(ev.buf, filetype, filename, false)
         end, {
           desc = "Convert pod with podium",
           nargs = "?",
           force = true,
         })
         vim.api.nvim_buf_create_user_command(ev.buf, "PodiumPreview", function(opts)
-          processToBuffer(ev.buf, opts.args, true)
+          local filetype, filename = preprocess(ev.buf, opts.args)
+          processToBuffer(ev.buf, filetype, filename, false)
         end, {
           desc = "Convert pod with podium and write it to file",
           nargs = "?",
