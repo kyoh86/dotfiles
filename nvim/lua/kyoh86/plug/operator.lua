33fafcec5cd729ceac9bcf4f95d68d6b8318be3b
kyoh86 <me@kyoh86.dev>
2023-08-09 01:18:13 +0900

    fix

--- nvim/lua/kyoh86/plug/operator.lua
+++ nvim/lua/kyoh86/plug/operator.lua
@@ -1,12 +1,3 @@
-vim.g.textobj_generics = { -- iG, aG (function call)で使う
-  {
-    ["header"] = "\\<\\%(\\h\\k*\\.\\)*\\h\\k*",
-    ["bra"] = "<",
-    ["ket"] = ">",
-    ["footer"] = "",
-  },
-}
-
 ---@type LazySpec[]
 local spec = {
   {
@@ -56,30 +47,55 @@ local spec = {
         command = { "startinsert" },
         input = { vim.keycode("<c-f>") },
       })
+      table.insert(recipes, {
+        buns = { "<", '">"' },
+        kind = { "add" },
+        cursor = "head",
+        command = { "startinsert" },
+        input = { "g" },
+      })
+      table.insert(recipes, {
+        external = { "i<", vim.keycode([[<plug>(textobj-functioncall-generics-a)]]) },
+        noremap = 0,
+        kind = { "delete", "replace", "query" },
+        input = { "g" },
+      })
       vim.g["operator#sandwich#recipes"] = recipes
+      vim.keymap.set({ "x", "n" }, "s", "<nop>", { remap = false, desc = "sandwich operations" })
+      vim.keymap.set("n", "sfa", "<plug>(operator-sandwich-add-query1st)<c-f>", { desc = "wrap target as argument" })
+      vim.keymap.set("x", "sfa", "<plug>(operator-sandwich-add)<c-f>", { desc = "wrap target as argument" })
+      vim.keymap.set("n", "sc", "<plug>(operator-sandwich-replace)<plug>(operator-sandwich-release-count)<plug>(textobj-sandwich-query-a)", { silent = true, desc = "replace sandwich" })
+      vim.keymap.set("n", "scb", "<plug>(operator-sandwich-replace)<plug>(operator-sandwich-release-count)<plug>(textobj-sandwich-auto-a)", { silent = true, desc = "replace sandwich for the typical brackets" })
     end,
-    keys = {
-      -- ignore s instead of the cl
-      { "s", "<nop>", remap = false, desc = "sandwich operations" },
-      { "s", "<nop>", mode = "x", remap = false, desc = "sandwich operations" },
-
-      { "sfa", "<plug>(operator-sandwich-add-query1st)<c-f>", desc = "wrap target as argument" },
-      { "sfa", "<plug>(operator-sandwich-add)<c-f>", mode = "x", desc = "wrap target as argument" },
-
-      { "sc", "<plug>(operator-sandwich-replace)<plug>(operator-sandwich-release-count)<plug>(textobj-sandwich-query-a)", silent = true, desc = "replace sandwich" },
-      { "scb", "<plug>(operator-sandwich-replace)<plug>(operator-sandwich-release-count)<plug>(textobj-sandwich-auto-a)", silent = true, desc = "replace sandwich for the typical brackets" },
-    },
+    dependencies = { "vim-textobj-functioncall" },
   },
   {
     "machakann/vim-textobj-functioncall",
-    keys = {
+    config = function()
+      vim.g.textobj_functioncall_generics_patterns = {
+        {
+          header = [[\<\%(\h\k*\.\)*\h\k*]],
+          bra = "<",
+          ket = ">",
+          footer = "",
+        },
+      }
+
       -- Genericsのカッコ（<>）絡み
-      { "iG", '<cmd>call textobj#functioncall#ip("o", g:textobj_generics)<cr>', mode = { "x", "o" }, desc = "a textobj in the brackets for the generics (<>)" },
-      { "aG", '<cmd>call textobj#functioncall#i("o", g:textobj_generics)<cr>', mode = { "x", "o" }, desc = "a textobj around the brackets for the generics (<>)" },
+      vim.keymap.set("o", "<plug>(textobj-functioncall-generics-i)", ":<c-u>call textobj#functioncall#ip('o', g:textobj_functioncall_generics_patterns)<cr>", { remap = false })
+      vim.keymap.set("x", "<plug>(textobj-functioncall-generics-i)", ":<c-u>call textobj#functioncall#ip('x', g:textobj_functioncall_generics_patterns)<cr>", { remap = false })
+      vim.keymap.set("o", "<plug>(textobj-functioncall-generics-a)", ":<c-u>call textobj#functioncall#i('o', g:textobj_functioncall_generics_patterns)<cr>", { remap = false })
+      vim.keymap.set("x", "<plug>(textobj-functioncall-generics-a)", ":<c-u>call textobj#functioncall#i('x', g:textobj_functioncall_generics_patterns)<cr>", { remap = false })
+
+      vim.keymap.set("o", "ig", "<Plug>(textobj-functioncall-generics-i)", {})
+      vim.keymap.set("x", "ig", "<Plug>(textobj-functioncall-generics-i)", {})
+      vim.keymap.set("o", "ag", "<Plug>(textobj-functioncall-generics-a)", {})
+      vim.keymap.set("x", "ag", "<Plug>(textobj-functioncall-generics-a)", {})
+
       -- Functionの呼び出し絡み
-      { "iF", "<plug>(textobj-functioncall-innerparen-i)", mode = { "x", "o" }, desc = "a textobj in the function calling" },
-      { "aF", "<plug>(textobj-functioncall-a)", mode = { "x", "o" }, desc = "a textobj around the function calling" },
-    },
+      vim.keymap.set({ "o", "x" }, "iF", "<plug>(textobj-functioncall-innerparen-i)", { silent = true, remap = false, desc = "a textobj in the function calling" })
+      vim.keymap.set({ "o", "x" }, "aF", "<plug>(textobj-functioncall-a)", { silent = true, remap = false, desc = "a textobj around the function calling" })
+    end,
     dependencies = { "vim-textobj-user" },
   },
   {
