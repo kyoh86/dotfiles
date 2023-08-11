54a03c46e61f0e93bbf1329835bf0c5ce97580ae
kyoh86 <me@kyoh86.dev>
2023-08-11 15:09:16 +0900

    select branch, select commit and cherry-pick it by ddu

--- nvim/lua/kyoh86/plug/ddu/source/git_branch.lua
+++ nvim/lua/kyoh86/plug/ddu/source/git_branch.lua
@@ -33,8 +33,33 @@ local spec = {
         ["<leader>d"] = { action = "itemAction", params = { name = "delete" } },
         ["<leader>r"] = { action = "itemAction", params = { name = "reset" } },
         ["<leader>c"] = { action = "itemAction", params = { name = "copy" } },
+        ["<leader>l"] = { action = "itemAction", params = { name = "custom:logs" } },
+
+        ["<leader>f"] = { action = "itemAction", params = { name = "fixupTo" } },
+        ["<leader>y"] = { action = "itemAction", params = { name = "yank" } },
+        ["<leader>p"] = { action = "itemAction", params = { name = "append" } },
       },
     })
+
+    local custom_logs = function(args)
+      if #args.items ~= 1 then
+        vim.notify("invalid action: it can edit only one file at once", vim.log.levels.WARN, {})
+        return 1
+      end
+      local refName = args.items[1].action.refName
+      local branch = refName.branch
+      if refName.remote ~= "" then
+        branch = refName.remote .. "/" .. branch
+      end
+      kyoh86.fa.ddu.start({
+        name = args.options.name,
+        push = true,
+        sources = { { name = "git_log", params = { startingCommit = { branch } } } },
+      })
+      return 0
+    end
+
+    kyoh86.fa.ddu.custom.action("kind", "git_branch", "custom:logs", custom_logs)
   end,
 }
 return spec
