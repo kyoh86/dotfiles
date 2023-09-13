711a2c9a782e4cc92d485ce705160067247ee0c2
kyoh86 <me@kyoh86.dev>
2023-09-13 22:03:00 +0900

    add docbase plugins

---
+++ nvim/lua/kyoh86/plug/ddu/source/docbase.lua
@@ -0,0 +1,23 @@
+local helper = require("kyoh86.plug.ddu.helper")
+
+---@type LazySpec
+local spec = {
+  "kyoh86/ddu-source-docbase",
+  config = function()
+    helper.setup("docbase_posts", {
+      sources = { { name = "docbase_posts", params = { domain = "wacul" } } },
+      kindOptions = {
+        file = {
+          defaultAction = "open",
+        },
+      },
+    }, {
+      startkey = "<leader>fd",
+      filelike = true,
+      localmap = {
+        ["<leader>b"] = { action = "itemAction", params = { name = "browse" } },
+      },
+    })
+  end,
+}
+return spec
---
+++ nvim/lua/kyoh86/plug/docbase.lua
@@ -0,0 +1,6 @@
+---@type LazySpec
+local spec = {
+  "kyoh86/denops-docbase.vim",
+  dependencies = { "denops.vim" },
+}
+return spec
