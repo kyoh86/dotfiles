859bbf7204d54dc81572a74a2cd62ba8dc52862b
kyoh86 <me@kyoh86.dev>
2023-09-20 11:09:48 +0900

    set dependencies for ddu-source-docbase

--- nvim/lua/kyoh86/plug/ddu/source/docbase.lua
+++ nvim/lua/kyoh86/plug/ddu/source/docbase.lua
@@ -3,6 +3,7 @@ local helper = require("kyoh86.plug.ddu.helper")
 ---@type LazySpec
 local spec = {
   "kyoh86/ddu-source-docbase",
+  dependencies = { "ddu.vim", "denops-docbase.vim" },
   config = function()
     helper.setup("docbase_posts", {
       sources = { { name = "docbase_posts", params = { domain = "wacul" } } },
