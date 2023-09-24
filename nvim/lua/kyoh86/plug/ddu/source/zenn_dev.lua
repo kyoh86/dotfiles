e2781327b1d6a02ee6f2f4eb23f6c73c7e61ae5d
kyoh86 <me@kyoh86.dev>
2023-09-25 03:01:16 +0900

    set date info for each zenn-dev articles in ddu

--- nvim/lua/kyoh86/plug/ddu/source/zenn_dev.lua
+++ nvim/lua/kyoh86/plug/ddu/source/zenn_dev.lua
@@ -2,40 +2,47 @@ local helper = require("kyoh86.plug.ddu.helper")
 
 ---@type LazySpec
 local spec = {
-  "kyoh86/ddu-source-zenn_dev",
-  dependencies = { "ddu.vim", "ddu-kind-file", "ddu-filter-kensaku" },
-  config = function()
-    helper.setup("zenn-dev-article", {
-      sources = { {
-        name = "zenn_dev_article",
-      } },
-      sourceOptions = {
-        _ = {
-          matchers = { "merge" },
-          columns = { "zenn_dev_slug", "zenn_dev_emoji", "zenn_dev_title" },
-        },
-      },
-      kindOptions = {
-        zenn_dev_article = {
-          defaultAction = "open",
-        },
-      },
-      filterParams = {
-        merge = {
-          filters = {
-            { name = "matcher_kensaku", weight = 1 },
-            { name = "matcher_fzf", weight = 1 },
+  {
+    "kyoh86/ddu-source-zenn_dev",
+    dependencies = { "ddu.vim", "ddu-kind-file", "ddu-filter-kensaku", "ddu-filter-sorter_alpha" },
+    config = function()
+      helper.setup("zenn-dev-article", {
+        sources = { {
+          name = "zenn_dev_article",
+        } },
+        sourceOptions = {
+          _ = {
+            matchers = { "merge" },
+            columns = { "zenn_dev_date", "zenn_dev_emoji", "zenn_dev_title" },
+            sorters = { "sorter_alpha", "sorter_fzf" },
           },
-          unique = true,
         },
-      },
-    }, {
-      startkey = "<leader>fza",
-      filelike = true,
-      localmap = {
-        ["<leader>b"] = { action = "itemAction", params = { name = "browse" } },
-      },
-    })
-  end,
+        kindOptions = {
+          zenn_dev_article = {
+            defaultAction = "open",
+          },
+        },
+        filterParams = {
+          merge = {
+            filters = {
+              { name = "matcher_kensaku", weight = 1 },
+              { name = "matcher_fzf", weight = 1 },
+            },
+            unique = true,
+          },
+        },
+      }, {
+        startkey = "<leader>fza",
+        filelike = true,
+        localmap = {
+          ["<leader>b"] = { action = "itemAction", params = { name = "browse" } },
+        },
+      })
+    end,
+  },
+  {
+    "Shougo/ddu-filter-sorter_alpha",
+    dependencies = { "ddu.vim" },
+  },
 }
 return spec
