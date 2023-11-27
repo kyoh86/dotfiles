d796f19182bdf8b076ff77a2a1d02fea329789e0
kyoh86 <me@kyoh86.dev>
2023-11-27 09:27:33 +0900

    use mini.align instead of align.nvim

--- nvim/lua/kyoh86/plug/align.lua
+++ nvim/lua/kyoh86/plug/align.lua
@@ -1,30 +1,9 @@
 ---@type LazySpec
 local spec = {
-  "Vonr/align.nvim",
-  config = function()
-    --
-    vim.keymap.set("x", "<leader>ac", function()
-      require("align").align_to_char(1, true)
-    end, { desc = "aligns to 1 character, looking left" })
-    vim.keymap.set("x", "<leader>as", function()
-      require("align").align_to_string(false, true, true)
-    end, { desc = "aligns to a string, looking left and with previews" })
-    vim.keymap.set("x", "<leader>ar", function()
-      require("align").align_to_string(true, true, true)
-    end, { desc = "aligns to a Lua pattern, looking left and with previews" })
-
-    vim.keymap.set("n", "gac", function()
-      local a = require("align")
-      a.operator(a.align_to_char, { length = 1, reverse = true })
-    end, { desc = "aling a textobj to 1 character, looking left" })
-    vim.keymap.set("n", "<leader>as", function()
-      local a = require("align")
-      a.operator(a.align_to_string, { is_pattern = false, reverse = true, preview = true })
-    end, { desc = "align a textobj to a string, looking left and with previews" })
-    vim.keymap.set("n", "<leader>ar", function()
-      local a = require("align")
-      a.operator(a.align_to_string, { is_pattern = true, reverse = true, preview = true })
-    end, { desc = "align a textobj to a Lua pattern, looking left and with previews" })
-  end,
+  "echasnovski/mini.align",
+  opt = { mappings = {
+    start = "<leader>a",
+    start_with_preview = "<leader>A",
+  } },
 }
 return spec
