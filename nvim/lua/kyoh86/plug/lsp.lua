06bc620cd73798225d1e99e7e325e3891a24ecb5
kyoh86 <me@kyoh86.dev>
2023-11-27 10:03:13 +0900

    disable vtsls for deno

--- nvim/lua/kyoh86/plug/lsp.lua
+++ nvim/lua/kyoh86/plug/lsp.lua
@@ -439,10 +439,10 @@ local function register_lsp_servers()
     root_dir = function(path)
       local marker = require("climbdir.marker")
       return require("climbdir").climb(path, marker.one_of(marker.has_readable_file("package.json"), marker.has_directory("node_modules")), {
-        halt = marker.has_readable_file("deno.json"),
+        halt = marker.one_of(marker.has_readable_file("deno.json"), marker.has_readable_file("deno.jsonc"), marker.has_directory("denops")),
       })
     end,
-    single_file_support = true,
+    single_file_support = false,
     filetypes = {
       "javascript",
       "javascriptreact",
