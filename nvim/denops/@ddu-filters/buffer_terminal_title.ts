9126ef8025612b2a7ef800c50b1453e21b1ba40e
kyoh86 <me@kyoh86.dev>
2023-12-08 22:17:47 +0900

    fix: buffer title

--- nvim/denops/@ddu-filters/buffer_terminal_title.ts
+++ nvim/denops/@ddu-filters/buffer_terminal_title.ts
@@ -1,17 +1,9 @@
 import { FilterArguments } from "https://deno.land/x/ddu_vim@v3.4.5/base/filter.ts";
-import { buffers } from "https://deno.land/x/denops_std@v5.0.2/variable/mod.ts";
-import { ensure } from "https://deno.land/x/denops_std@v5.0.2/buffer/mod.ts";
+import { getbufvar } from "https://deno.land/x/denops_std@v5.0.2/function/buffer.ts";
 import {
   BaseFilter,
   DduItem,
-  Item,
-  ItemHighlight,
 } from "https://deno.land/x/ddu_vim@v3.4.5/types.ts";
-
-type ActionData = {
-  bufNr: number;
-  isTerminal: boolean;
-};
 
 const defaultParams = {
   highlightAdded: "diffAdded",
@@ -22,18 +14,18 @@ type Params = typeof defaultParams;
 
 export class Filter extends BaseFilter<Params> {
   async filter(args: FilterArguments<Params>): Promise<DduItem[]> {
-    const items = args.items as Item<ActionData>[];
-    const newItems = await Promise.all(items.map(async (item) => {
-      if (!item.action?.isTerminal) {
+    const newItems = await Promise.all(args.items.map(async (item) => {
+      const action = item.action! as {
+        bufNr: number;
+        isTerminal: boolean;
+      };
+      if (!action.isTerminal) {
         return item;
       }
-
-      await ensure(args.denops, item.action.bufNr, async () => {
-        const title = await buffers.get(args.denops, "term_title", "");
-        if (title !== "") {
-          item.word = item.word.replace(/term:\/\/.*/, `term://${title}`);
-        }
-      });
+      const title = await getbufvar(args.denops, action.bufNr, "term_title");
+      if (title !== "") {
+        item.word = item.word.replace(/term:\/\/.*/, `term://${title}`);
+      }
       return item;
     }));
     return Promise.resolve(newItems as DduItem[]);
