f234ffdc362634e2b319cfa6dcbd1160ee31dbed
kyoh86 <me@kyoh86.dev>
2023-12-11 22:43:28 +0900

    generate ddu static & restore it before update fix: #57

--- nvim/lua/kyoh86/plug/ddu/init.lua
+++ nvim/lua/kyoh86/plug/ddu/init.lua
@@ -2,15 +2,22 @@ local func = require("kyoh86.lib.func")
 ---@type LazySpec[]
 local spec = {
   {
-    --
     "Shougo/ddu.vim",
-    config = function()
+    config = function(plugin)
       local group = vim.api.nvim_create_augroup("kyoh86-plug-ddu-static-import", { clear = true })
       vim.api.nvim_create_autocmd("User", {
         group = group,
-        pattern = { "LazySync", "LazyInstall", "LazyUpdate", "LazyClean" },
+        pattern = { "LazyUpdate" },
         callback = function()
           vim.fn["ddu#set_static_import_path"]()
+        end,
+      })
+      vim.api.nvim_create_autocmd("User", {
+        group = group,
+        pattern = { "LazyUpdatePre" },
+        callback = function()
+          local cmd = string.format("git -C %q restore denops/ddu/_mods.js", plugin.dir)
+          vim.fn.system(cmd)
         end,
       })
       vim.fn["ddu#custom#patch_global"]({
