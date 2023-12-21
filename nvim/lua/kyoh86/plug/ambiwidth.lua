fd1a6c28904c086300349549e9da0a786033b342
kyoh86 <me@kyoh86.dev>
2023-12-21 21:43:37 +0900

    bye-bye, vim-ambiwidth. glad to meet you, delphinus/cellwidths.nvim
    fix: #65

--- nvim/lua/kyoh86/plug/ambiwidth.lua
+++ nvim/lua/kyoh86/plug/ambiwidth.lua
@@ -1,31 +1,38 @@
 ---@type LazySpec
 local spec = {
-  "rbtnn/vim-ambiwidth",
-  init = function()
-    vim.g.ambiwidth_cica_enabled = false
-    vim.g.ambiwidth_add_list = {
-      { 0x23fb, 0x23fe, 2 },
-      { 0xe000, 0xe00a, 2 },
-      { 0xe0a0, 0xe0a2, 2 },
-      { 0xe0a3, 0xe0a3, 2 },
-      { 0xe0b0, 0xe0b3, 2 },
-      { 0xe0b4, 0xe0c8, 2 },
-      { 0xe0ca, 0xe0ca, 2 },
-      { 0xe0cc, 0xe0d4, 2 },
-      { 0xe200, 0xe2a9, 2 },
-      { 0xe300, 0xe3eb, 2 },
-      { 0xe5fa, 0xe62d, 2 },
-      { 0xe62f, 0xe631, 2 },
-      { 0xe700, 0xe7c5, 2 },
-      { 0xea60, 0xebeb, 2 },
-      { 0xf000, 0xf2e0, 2 },
-      { 0xf300, 0xf314, 2 },
-      { 0xf317, 0xf31a, 2 },
-      { 0xf31d, 0xf32f, 2 },
-      { 0xf400, 0xf4a8, 2 },
-      { 0xf4a9, 0xf4a9, 2 },
-      { 0xf500, 0xfd46, 2 },
-    }
+  "delphinus/cellwidths.nvim",
+  config = function()
+    require("cellwidths").setup({
+      name = "user/custom",
+      fallback = function(cw)
+        cw.load("default")
+
+        cw.add({ 0x23FB, 0x23FE, 2 })
+        cw.add({ 0xE000, 0xE00A, 2 })
+        cw.add({ 0xE0A0, 0xE0A2, 2 })
+        cw.add({ 0xE0A3, 0xE0A3, 2 })
+        cw.add({ 0xE0B0, 0xE0B3, 2 })
+        cw.add({ 0xE0B4, 0xE0C8, 2 })
+        cw.add({ 0xE0CA, 0xE0CA, 2 })
+        cw.add({ 0xE0CC, 0xE0D4, 2 })
+        cw.add({ 0xE200, 0xE2A9, 2 })
+        cw.add({ 0xE300, 0xE3EB, 2 })
+        cw.add({ 0xE5FA, 0xE62D, 2 })
+        cw.add({ 0xE62F, 0xE631, 2 })
+        cw.add({ 0xE700, 0xE7C5, 2 })
+        cw.add({ 0xEA60, 0xEBEB, 2 })
+        cw.add({ 0xF000, 0xF2E0, 2 })
+        cw.add({ 0xF300, 0xF314, 2 })
+        cw.add({ 0xF317, 0xF31A, 2 })
+        cw.add({ 0xF31D, 0xF32F, 2 })
+        cw.add({ 0xF400, 0xF4A8, 2 })
+        cw.add({ 0xF4A9, 0xF4A9, 2 })
+        cw.add({ 0xF500, 0xFD46, 2 })
+
+        cw.delete({ 0x25B2, 0x25B3 })
+      end,
+      build = ":CellWidthsRemove",
+    })
   end,
 }
 return spec
