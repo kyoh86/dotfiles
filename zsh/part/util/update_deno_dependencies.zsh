77f0e74c442c5a9bda0bc5168396ac470a430323
kyoh86 <me@kyoh86.dev>
2024-01-17 23:33:32 +0900

    fix update_deno_dependencies

--- zsh/part/util/update_deno_dependencies.zsh
+++ zsh/part/util/update_deno_dependencies.zsh
@@ -3,48 +3,48 @@ function _update_deno_dependencies() {
   echo "\e[1m\e[31mUpdating $dir...\e[0m"
   if [ -n "$(git -C "$dir" status --porcelain)" ]; then
     echo "\e[31mThere're changes in $dir\e[0m"
-    continue
+    return
   fi
   echo "Pulling $dir..."
   git -C "$dir" pull
   if [ -n "$(git -C "$dir" status --porcelain)" ]; then
     echo "\e[31mThere're changes in $dir\e[0m"
-    continue
+    return
   fi
   echo "No dirty changes in $dir"
   if ! NO_COLOR=1 udd "./$dir/"**/*.ts; then
     echo "\e[31mFailed to update dependencies in $dir\e[0m"
-    continue
+    return
   fi
   if [ -z "$(git -C "$dir" status --porcelain)" ]; then
     echo "There's no update in $dir"
-    continue
+    return
   fi
   if ! NO_COLOR=1 deno cache "./$dir/"**/*.ts; then
     echo "\e[31mFailed to cache in $dir\e[0m"
-    continue
+    return
   fi
   if ! NO_COLOR=1 deno task lint; then
     echo "\e[31mThere're lints in $dir\e[0m"
-    continue
+    return
   fi
   if [[ -n "./$dir/"**/*_test.ts(#qN) ]]; then
     if ! NO_COLOR=1 deno task test; then
       echo "\e[31mFailed to test $dir\e[0m"
-      continue
+      return
     fi
   fi
   if ! git -C "$dir" add .; then
     echo "\e[31mFailed to stage changes in $dir\e[0m"
-    continue
+    return
   fi
   if ! git -C "$dir" commit -m "Update dependencies"; then
     echo "\e[31mFailed to commit changes in $dir\e[0m"
-    continue
+    return
   fi
   if ! git -C "$dir" push; then
     echo "\e[31mFailed to push changes in $dir\e[0m"
-    continue
+    return
   fi
 }
 
