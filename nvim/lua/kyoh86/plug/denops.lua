73984b29673858ebfd8932843a9885682784e38f
kyoh86 <me@kyoh86.dev>
2024-01-26 10:18:11 +0900

    "--unstable-kv"

--- nvim/lua/kyoh86/plug/denops.lua
+++ nvim/lua/kyoh86/plug/denops.lua
@@ -4,6 +4,7 @@ local spec = {
   config = function(plugin)
     -- vim.g["denops#debug"] = 1
     vim.env.DENOPS_TEST_DENOPS_PATH = plugin.dir
+    vim.g["denops#server#deno_args"] = { "-q", "--no-lock", "-A", "--unstable-kv" }
   end,
 }
 return spec
