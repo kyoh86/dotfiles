6863a50c237a3f69d24b5905dd78a15ac8673958
kyoh86 <me@kyoh86.dev>
2024-01-26 14:23:46 +0900

    enhancement clipboard cost

--- nvim/lua/kyoh86/conf/clipboard.lua
+++ nvim/lua/kyoh86/conf/clipboard.lua
@@ -1,13 +1,17 @@
-vim.opt.clipboard = "unnamedplus,unnamed"
+vim.opt.clipboard = "unnamed"
 vim.g.clipboard = {
   name = "win32yank-wsl",
   copy = {
     ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
+    ["*"] = function(lines, regtype)
+      vim.g["kyoh86_clip_aster"] = { lines, regtype }
+    end,
   },
   paste = {
     ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
+    ["*"] = function(lines, regtype)
+      return vim.g["kyoh86_clip_aster"] or {}
+    end,
   },
-  cache_enabled = 0,
+  cache_enabled = 1,
 }
