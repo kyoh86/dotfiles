46649f68041c313e8a503bdf009371edea1772b4
kyoh86 <me@kyoh86.dev>
2024-01-27 10:09:15 +0900

    delay loading schemastore

--- nvim/lua/kyoh86/plug/lsp/jsonls.lua
+++ nvim/lua/kyoh86/plug/lsp/jsonls.lua
@@ -1,19 +1,21 @@
-local schemas = require("schemastore").json.schemas()
-table.insert(schemas, {
-  description = "JSON schema for VSCode Code Snippets",
-  fileMatch = { "nvim/vsnip/*.json" },
-  url = "https://raw.githubusercontent.com/Yash-Singh1/vscode-snippets-json-schema/main/schema.json",
-})
-table.insert(schemas, {
-  description = "Google Tag Manager",
-  fileMatch = "GTM-*_workspace*.json",
-  url = vim.fn.stdpath("config") .. "/schema/gtm.json",
-})
 return {
-  settings = {
-    json = {
-      schemas = schemas,
-      validate = { enable = true },
-    },
-  },
+  on_init = function(client)
+    local schemas = require("schemastore").json.schemas()
+    table.insert(schemas, {
+      description = "JSON schema for VSCode Code Snippets",
+      fileMatch = { "nvim/vsnip/*.json" },
+      url = "https://raw.githubusercontent.com/Yash-Singh1/vscode-snippets-json-schema/main/schema.json",
+    })
+    table.insert(schemas, {
+      description = "Google Tag Manager",
+      fileMatch = "GTM-*_workspace*.json",
+      url = vim.fn.stdpath("config") .. "/schema/gtm.json",
+    })
+    client.config.settings = vim.tbl_deep_extend("force", client.config.settings or {}, {
+      json = {
+        schemas = schemas,
+        validate = { enable = true },
+      },
+    })
+  end,
 }
