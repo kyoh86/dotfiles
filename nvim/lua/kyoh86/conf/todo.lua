0e4c6c1540b46b9a46b10ec8c5b89ba96a182f08
kyoh86 <me@kyoh86.dev>
2024-02-01 20:13:40 +0900

    open Todo in same window

--- nvim/lua/kyoh86/conf/todo.lua
+++ nvim/lua/kyoh86/conf/todo.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>tit", [[<cmd>Ripgrep -i to]]..[[do<cr>]], {remap=false, desc="To".."Doを検索する"})
-vim.keymap.set("n", "<leader><leader>t", [[<cmd>tabedit ~/.local/state/to]]..[[do.md<cr>]], {remap=false, desc="作業メモを編集する"})
+vim.keymap.set("n", "<leader>tit", [[<cmd>Ripgrep -i to]] .. [[do<cr>]], { remap = false, desc = "To" .. "Doを検索する" })
+vim.keymap.set("n", "<leader><leader>t", [[<cmd>new ~/.local/state/to]] .. [[do.md<cr>]], { remap = false, desc = "作業メモを編集する" })
