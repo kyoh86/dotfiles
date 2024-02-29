c6fb1e9d9aaa3dc8c00c269ab2907489e7d1dae7
kyoh86 <me@kyoh86.dev>
2024-02-29 20:47:34 +0900

    fit todo window size

--- nvim/lua/kyoh86/conf/todo.lua
+++ nvim/lua/kyoh86/conf/todo.lua
@@ -1,2 +1,12 @@
-vim.keymap.set("n", "<leader>tit", [[<cmd>Ripgrep -i to]] .. [[do<cr>]], { remap = false, desc = "To" .. "Doを検索する" })
-vim.keymap.set("n", "<leader><leader>t", [[<cmd>new ~/.local/state/to]] .. [[do.md<cr>]], { remap = false, desc = "作業メモを編集する" })
+vim.keymap.set("n", "<leader>tit",
+  [[<cmd>Ripgrep -i to]] .. [[do<cr><cmd>lua require("kyoh86.conf.todo").resize_window_to_fit_content()<cr>]],
+  { remap = false, desc = "To" .. "Doを検索する" })
+vim.keymap.set("n", "<leader><leader>t",
+  [[<cmd>new ~/.local/state/to]] .. [[do.md<cr><cmd>lua require("kyoh86.conf.todo").resize_window_to_fit_content()<cr>]],
+  { remap = false, desc = "作業メモを編集する" })
+return {
+  resize_window_to_fit_content = function()
+    local line_count = vim.api.nvim_buf_line_count(0)
+    vim.api.nvim_win_set_height(0, line_count + 1)
+  end,
+}
