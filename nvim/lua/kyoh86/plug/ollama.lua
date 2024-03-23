6429c0736a30ea2d5f94bf7d065293fe80e1f0a6
kyoh86 <me@kyoh86.dev>
2024-03-23 17:38:23 +0900

    wip: use tailscale for ddc

--- nvim/lua/kyoh86/plug/ollama.lua
+++ nvim/lua/kyoh86/plug/ollama.lua
@@ -2,5 +2,12 @@
 local spec = {
   "kyoh86/denops-ollama.vim",
   dev=true,
+  config = function()
+    -- vim.fn['denops#server#wait']()
+    vim.fn['denops#plugin#wait_async']('ollama', function()
+      vim.fn['ollama#custom#patch_func_args']('complete', {model= 'codellama', baseUrl= "http://100.96.204.129:11434"})
+    end)
+  end,
+  dependencies = {"denops.vim"},
 }
 return spec
