1656fff2b251e7ac354e78240045ae09e27c3073
kyoh86 <me@kyoh86.dev>
2024-04-11 10:46:17 +0900

    wip

--- nvim/lua/kyoh86/plug/ollama.lua
+++ nvim/lua/kyoh86/plug/ollama.lua
@@ -1,13 +1,12 @@
 ---@type LazySpec
 local spec = {
   "kyoh86/denops-ollama.vim",
-  dev=true,
   config = function()
     -- vim.fn['denops#server#wait']()
-    vim.fn['denops#plugin#wait_async']('ollama', function()
-      vim.fn['ollama#custom#patch_func_args']('complete', {model= 'codellama', baseUrl= "http://127.0.0.1:11434"})
+    vim.fn["denops#plugin#wait_async"]("ollama", function()
+      vim.fn["ollama#custom#patch_func_args"]("complete", { model = "codellama", baseUrl = "http://127.0.0.1:11434" })
     end)
   end,
-  dependencies = {"denops.vim"},
+  dependencies = { "denops.vim" },
 }
 return spec
---
+++ nvim/lua/kyoh86/plug/zenn_dev.lua
@@ -0,0 +1,6 @@
+---@type LazySpec
+local spec = {
+  "kyoh86/denops-zenn_dev.vim",
+  dependencies = { "denops.vim" },
+}
+return spec
