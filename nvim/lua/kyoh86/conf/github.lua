5b0763a78ad718c536947ab066accb5e58c9250c
kyoh86 <me@kyoh86.dev>
2024-05-19 11:48:04 +0900

    enhance GitHub manipulations

--- nvim/lua/kyoh86/conf/github.lua
+++ nvim/lua/kyoh86/conf/github.lua
@@ -1,18 +1,34 @@
 -- GitHub PRの作成
-vim.keymap.set("n", "<leader>ghp", function()
-  require("kyoh86.lib.volatile_terminal").split(0, {}, {
-    exec = "gh pr new",
-  })
-end, { remap = false, desc = "Create new pull-request in the current repository on GitHub" })
+vim.api.nvim_create_user_command("GitHubPullRequest", function(opts)
+  local args = opts.args or {}
+  local exec = "gh pr new"
+  if #args == 1 then
+    exec = "gh pr new --title " .. args[1]
+  end
+  require("kyoh86.lib.volatile_terminal").split(0, {}, { exec = exec })
+end, { nargs = "?", desc = "Create new pull-request in the current repository on GitHub" })
 
 -- GitHub Issue の作成
-vim.keymap.set("n", "<leader>ghi", function()
-  require("kyoh86.lib.volatile_terminal").split(0, {}, {
-    exec = "gh issue new",
-  })
-end, { remap = false, desc = "Create new issue in the current repository on GitHub" })
 
--- GitHub Issue をタイトル付きで作成
+vim.api.nvim_create_user_command("GitHubIssue", function(opts)
+  local args = opts.args or {}
+  local exec = "gh issue new"
+  if #args == 1 then
+    exec = "gh issue new --title " .. args[1]
+  elseif opts.range > 0 then
+    local head = vim.fn.getpos("'<")
+    local tail = vim.fn.getpos("'>")
+    local lines = vim.fn.getregion(head, tail, { type = vim.fn.visualmode() })
+    local title = lines[1]
+    if #lines > 1 then
+      local body = table.concat(lines, "\\n", 2)
+      exec = "gh issue new --title " .. title .. " --body " .. body
+    else
+      exec = "gh issue new --title " .. title
+    end
+  end
+  require("kyoh86.lib.volatile_terminal").split(0, {}, { exec = exec })
+end, { nargs = "?", range = true, desc = "Create new pull-request in the current repository on GitHub" })
 
 -- オペレータ関数
 function _G.create_github_issue_with_title()
@@ -30,5 +46,5 @@ function _G.create_github_issue_with_title()
 end
 
 -- オペレータの設定
-vim.api.nvim_set_keymap("n", "<Leader>ghit", "<Cmd>set opfunc=v:lua.create_github_issue_with_title<CR>g@", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>ghi", "<Cmd>set opfunc=v:lua.create_github_issue_with_title<CR>g@", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "<Leader>ghi", "<Cmd>set opfunc=v:lua.create_github_issue_with_title<CR>g@", { noremap = true, silent = true })
