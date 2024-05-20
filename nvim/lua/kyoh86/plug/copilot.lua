0a625b16505ffd2cf2872c8d6d64eb919fa4e2b1
kyoh86 <me@kyoh86.dev>
2024-05-20 18:01:26 +0900

    fix keymap for copilot

--- nvim/lua/kyoh86/plug/copilot.lua
+++ nvim/lua/kyoh86/plug/copilot.lua
@@ -3,10 +3,9 @@ local spec = {
   "github/copilot.vim",
   init = function()
     vim.g.copilot_no_maps = true
-    vim.keymap.set("i", "<c-x><c-a>", function()
-      return vim.fn["copilot#Accept"]("<c-x><c-a>")
-    end, {
+    vim.keymap.set("i", "<c-x><c-a>", 'copilot#Accept("\\<c-x>\\<c-a>")', {
       expr = true,
+      replace_keycodes = false,
     })
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_filetypes = {
