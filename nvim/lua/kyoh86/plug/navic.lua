d0f12ed3e9bd52355949056339a979bc44537989
kyoh86 <me@kyoh86.dev>
2024-05-21 23:13:03 +0900

    fix: remove unused setting

--- nvim/lua/kyoh86/plug/navic.lua
+++ nvim/lua/kyoh86/plug/navic.lua
@@ -1,18 +1,6 @@
 ---@type LazySpec
 local spec = {
   "SmiteshP/nvim-navic",
-  dependencies = {"nvim-lspconfig"},
-  setup = function(use)
-    use({
-      "SmiteshP/nvim-navic",
-      config = function()
-        require("nvim-navic").setup({
-          on_attach = function(client, bufnr)
-            require("nvim-lspconfig").on_attach(client, bufnr)
-          end,
-        })
-      end,
-    })
-  end,
+  dependencies = { "nvim-lspconfig" },
 }
 return spec
