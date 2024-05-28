0fbf2059b7b90e5f11a2c00b181e9220852af6dd
kyoh86 <me@kyoh86.dev>
2024-05-28 11:38:01 +0900

    cache deno dependencies after lazy install/update

--- nvim/lua/kyoh86/plug/denops.lua
+++ nvim/lua/kyoh86/plug/denops.lua
@@ -5,6 +5,15 @@ local spec = {
     -- vim.g["denops#debug"] = 1
     vim.env.DENOPS_TEST_DENOPS_PATH = plugin.dir
     vim.g["denops#server#deno_args"] = { "-q", "--no-lock", "-A", "--unstable-kv" }
+    local group = vim.api.nvim_create_augroup("kyoh86-plug-denops-deno-cache", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = group,
+      pattern = { "LazyInstall", "LazyUpdate" },
+      callback = function()
+        vim.system({ "deno", "cache", kyoh86.lazydir("*/denops/**/*.ts") })
+        vim.notify("denops dependencies cached", vim.log.levels.INFO)
+      end,
+    })
   end,
 }
 return spec
