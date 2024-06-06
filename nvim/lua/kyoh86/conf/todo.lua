5121fef0fd01848a50d1b46544d0c46f47848b59
kyoh86 <me@kyoh86.dev>
2024-06-06 13:26:03 +0900

    never opens todo in duplicated window

--- nvim/lua/kyoh86/conf/todo.lua
+++ nvim/lua/kyoh86/conf/todo.lua
@@ -1,12 +1,16 @@
-vim.keymap.set("n", "<leader>tit",
-  [[<cmd>Ripgrep -i to]] .. [[do<cr><cmd>lua require("kyoh86.conf.todo").resize_window_to_fit_content()<cr>]],
-  { remap = false, desc = "To" .. "Doを検索する" })
-vim.keymap.set("n", "<leader><leader>t",
-  [[<cmd>new ~/.local/state/to]] .. [[do.md<cr><cmd>lua require("kyoh86.conf.todo").resize_window_to_fit_content()<cr>]],
-  { remap = false, desc = "作業メモを編集する" })
+vim.keymap.set("n", "<leader>tit", [[<cmd>Ripgrep -i to]] .. [[do<cr><cmd>lua require("kyoh86.conf.todo").resize_window_to_fit_content()<cr>]], { remap = false, desc = "To" .. "Doを検索する" })
+vim.keymap.set("n", "<leader><leader>t", [[<cmd>lua require("kyoh86.conf.todo").resize_window_to_fit_content()<cr>]], { remap = false, desc = "作業メモを編集する" })
+local filename = "~/.local/state/to" .. "do.md"
 return {
   resize_window_to_fit_content = function()
+    local winid = vim.fn.bufwinid(filename)
+    if winid < 0 then
+      vim.cmd([[topleft new ]] .. filename)
+      winid = vim.fn.bufwinid(filename)
+    else
+      vim.api.nvim_set_current_win(winid)
+    end
     local line_count = vim.api.nvim_buf_line_count(0)
-    vim.api.nvim_win_set_height(0, line_count + 1)
+    vim.api.nvim_win_set_height(winid, line_count + 1)
   end,
 }
