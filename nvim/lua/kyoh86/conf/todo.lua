4b9909eac6c84a358f07fea76f5a1722dd4689d2
kyoh86 <me@kyoh86.dev>
2024-06-06 16:05:41 +0900

    open or focus todo

--- nvim/lua/kyoh86/conf/todo.lua
+++ nvim/lua/kyoh86/conf/todo.lua
@@ -1,14 +1,17 @@
-vim.keymap.set("n", "<leader>tit", [[<cmd>Ripgrep -i to]] .. [[do<cr><cmd>lua require("kyoh86.conf.todo").resize_window_to_fit_content()<cr>]], { remap = false, desc = "To" .. "Doを検索する" })
-vim.keymap.set("n", "<leader><leader>t", [[<cmd>lua require("kyoh86.conf.todo").resize_window_to_fit_content()<cr>]], { remap = false, desc = "作業メモを編集する" })
+vim.keymap.set("n", "<leader>tit", [[<cmd>Ripgrep -i to]] .. [[do<cr>]], { remap = false, desc = "To" .. "Doを検索する" })
+vim.keymap.set("n", "<leader><leader>t", [[<cmd>lua require("kyoh86.conf.todo").open_note()<cr>]], { remap = false, desc = "作業メモを編集する" })
 local filename = "~/.local/state/to" .. "do.md"
 return {
-  resize_window_to_fit_content = function()
-    local winid = vim.fn.bufwinid(filename)
-    if winid < 0 then
+  open_note = function()
+    local winids = vim.fn.win_findbuf(vim.fn.bufnr(filename))
+    local winid = -1
+    if #winids == 0 then
       vim.cmd([[topleft new ]] .. filename)
       winid = vim.fn.bufwinid(filename)
     else
+      winid = winids[1]
       vim.api.nvim_set_current_win(winid)
+      vim.cmd([[wincmd K]])
     end
     local line_count = vim.api.nvim_buf_line_count(0)
     vim.api.nvim_win_set_height(winid, line_count + 1)
