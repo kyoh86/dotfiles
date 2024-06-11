71444e2487b9510803a5215d434a5425cd8d628d
kyoh86 <me@kyoh86.dev>
2024-06-12 00:11:02 +0900

    Call Ex commands from Shell

---
+++ zsh/part/nvim_cmd.zsh
@@ -0,0 +1,24 @@
+# ref: https://blog.atusy.net/2023/02/02/zsh-as-nvim-cmdline/
+if [ -n "${NVIM_SERVER_NAME}" ] ; then
+  function nvim-remote-or-accept-line() {
+    # バッファが:で始まる場合はNeovimに実行させ結果を表示する
+    if echo $BUFFER | grep --silent "^:"; then
+      # コマンドをNeovimに実行させ、出力を受けとる
+      local RES="$(nvim --server ${NVIM_SERVER_NAME} --headless --remote-expr "execute(\"${BUFFER//\"/\\\"}\")" )"
+
+      # 出力を表示する
+      printf "$RES"
+
+      # <C-C>っぽいことして現在のZshのバッファを終了する
+      zle send-break
+      return
+    fi
+
+    # Neovimに送らない時は普通に処理する
+    zle accept-line
+  }
+
+  # Enterキーでnvim-remote-or-accept-lineが発動するようにマッピング
+  zle -N nvim-remote-or-accept-line
+  bindkey '^m' nvim-remote-or-accept-line
+fi
