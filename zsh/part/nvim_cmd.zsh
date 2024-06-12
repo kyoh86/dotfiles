df24915731da6e58dec4ee800bf5291f1e2445de
kyoh86 <me@kyoh86.dev>
2024-06-12 17:38:50 +0900

    fix Ex commands from shell

--- zsh/part/nvim_cmd.zsh
+++ zsh/part/nvim_cmd.zsh
@@ -1,24 +1,44 @@
 # ref: https://blog.atusy.net/2023/02/02/zsh-as-nvim-cmdline/
 if [ -n "${NVIM_SERVER_NAME}" ] ; then
-  function nvim-remote-or-accept-line() {
-    # バッファが:で始まる場合はNeovimに実行させ結果を表示する
-    if [ "$BUFFER[1]" = ":" ]; then
-      # コマンドをNeovimに実行させ、出力を受けとる
-      local RES="$(nvim --server ${NVIM_SERVER_NAME} --headless --remote-expr "execute(v:lua.vim.base64.decode(\"$(echo "$BUFFER" | base64)\"))")"
+  function nvim-remote-cmd() {
+    local result="$(nvim --server "${NVIM_SERVER_NAME}" --headless --remote-expr "execute(v:lua.vim.base64.decode(\"$1\"))")"
+    print "$result" | tail -n +2
+  }
 
-      # 出力を表示する
-      printf "$RES"
-
-      # <C-C>っぽいことして現在のZshのバッファを終了する
-      zle send-break
+  function nvim-remote-or-not() {
+    if [ "$BUFFER[1]" != ":" ] || [ "$BUFFER" = ":" ]; then
+      zle accept-line
       return
     fi
 
-    # Neovimに送らない時は普通に処理する
+    # バッファが:で始まる場合はNeovimに実行させる
+
+    # コマンド名を取得
+    local cmd_name="${BUFFER%% *}"
+
+    # 一時的にコマンドをfunctionとして定義
+    eval "${cmd_name}() { nvim-remote-cmd \"$(echo $BUFFER|base64)\" }"
+
+    # 掃除用にプレフィックス関数の名前を保持
+    TEMP_NVIM_CMD_NAME="$cmd_name"
+
+    # 一時的に定義したコマンドのまま実行
     zle accept-line
   }
 
-  # Enterキーでnvim-remote-or-accept-lineが発動するようにマッピング
-  zle -N nvim-remote-or-accept-line
-  bindkey '^m' nvim-remote-or-accept-line
+  # Enterキーでnvim-remote-or-notが発動するようにマッピング
+  zle -N nvim-remote-or-not
+  bindkey '^m' nvim-remote-or-not
 fi
+
+# precmdフックを設定して、一時的な関数を削除
+function cleanup_temp_command() {
+  if [[ -n "$TEMP_NVIM_CMD_NAME" ]]; then
+    unset -f "$TEMP_NVIM_CMD_NAME"
+    unset TEMP_NVIM_CMD_NAME
+  fi
+}
+
+# precmd関数を追加
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd cleanup_temp_command
