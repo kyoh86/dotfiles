832079bbacf50a9879f9fc90fc2eaa8f135bf968
kyoh86 <me@kyoh86.dev>
2024-06-13 10:04:29 +0900

    add commands for docbase

--- nvim/lua/kyoh86/plug/docbase.lua
+++ nvim/lua/kyoh86/plug/docbase.lua
@@ -2,5 +2,14 @@
 local spec = {
   "kyoh86/denops-docbase.vim",
   dependencies = { "denops.vim" },
+  config = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "DenopsPluginPost:docbase",
+      callback = function()
+        vim.fn["docbase#setup#maps"]()
+        vim.fn["docbase#setup#commands"]()
+      end,
+    })
+  end,
 }
 return spec
