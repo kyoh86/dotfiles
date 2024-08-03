fc27907985025eb35b2b02a8f5ab2030f1a3c84a
kyoh86 <me@kyoh86.dev>
2024-08-04 01:07:22 +0900

    put dummy test for deno

---
+++ nvim/dummy_test.ts
@@ -0,0 +1,10 @@
+import { test } from "jsr:@denops/test@~3.0.1";
+import { assert } from "jsr:@std/assert@~1.0.1";
+
+test({
+  mode: "all",
+  name: "dummy",
+  fn: () => {
+    assert(true);
+  },
+});
