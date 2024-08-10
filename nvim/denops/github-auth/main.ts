73d1f47d00fd8b1d594665229c86dd68b4848940
kyoh86 <me@kyoh86.dev>
2024-08-10 21:59:43 +0900

    auth GitHub

---
+++ nvim/denops/github-auth/main.ts
@@ -0,0 +1,43 @@
+import type { Entrypoint } from "jsr:@denops/std@~7.0.1";
+import { batch } from "jsr:@denops/std@~7.0.1/batch";
+import { echo } from "jsr:@denops/std@~7.0.1/helper";
+import { systemopen } from "jsr:@lambdalisue/systemopen@~1.0.0";
+
+import {
+  createOAuthDeviceAuth,
+} from "https://esm.sh/@octokit/auth-oauth-device@7.1.1";
+import type { OnVerificationCallback } from "https://esm.sh/v135/@octokit/auth-oauth-device@7.1.1/dist-types/types.d.ts";
+
+const ClientID = "Iv23liIclzYxPQJBSI7d";
+
+export const main: Entrypoint = (denops) => {
+  denops.dispatcher = {
+    login: async () => {
+      const options: {
+        onVerification: OnVerificationCallback;
+      } = {
+        onVerification: (v) => {
+          batch(denops, async (denops) => {
+            await echo(
+              denops,
+              [
+                `Open ${v.verification_uri}`,
+                `and put your one-time code : ${v.user_code}`,
+              ].join("\n "),
+            );
+          });
+          systemopen(v.verification_uri);
+          // TODO: If it does not inistalled, kyoh86-dotfiles should be installed.
+          // https://github.com/settings/apps/kyoh86-dotfiles/installations
+        },
+      };
+
+      const auth = createOAuthDeviceAuth({
+        clientType: "github-app",
+        clientId: ClientID,
+        ...options,
+      });
+      return await auth({ type: "oauth" });
+    },
+  };
+};
--- nvim/lua/kyoh86/lib/glaze.lua
+++ nvim/lua/kyoh86/lib/glaze.lua
@@ -2,17 +2,17 @@ local Cache = require("kyoh86.lib.cache")
 local file = vim.fs.joinpath(vim.fn.stdpath("cache") --[[@as string]], "kyoh86-glaze.json")
 local cache = Cache.new(file)
 
---- 焼付型の設定: 判定に時間がかかる処理の結果を、初回起動時に焼き付ける。
----
+--- 設定が焼き付けられているか確認する
 ---@param name string 対象設定の名前
----@param get_variant fun():any 判定関数
-local function glaze(name, get_variant)
-  if cache:has(name) then
-    return
-  end
-  local value = get_variant()
-  cache:set(name, value)
-  return value
+local function has(name)
+  return cache:has(name)
+end
+
+--- 設定値を直接セットする
+---@param name string 対象設定の名前
+---@param value any 対象設定の値
+local function set(name, value)
+  return cache:set(name, value)
 end
 
 --- 焼き付けた内容を設定として使用する
@@ -22,6 +22,17 @@ local function get(name, callback)
   cache:get(name, callback)
 end
 
+--- 焼付型の設定: 判定に時間がかかる処理の結果を、初回起動時に焼き付ける。
+---
+---@param name string 対象設定の名前
+---@param get_variant fun():any 判定関数
+local function glaze(name, get_variant)
+  if cache:has(name) then
+    return
+  end
+  set(name, get_variant())
+end
+
 --- 焼き付けた内容を完全にリセットする
 local function reset()
   cache:clear()
@@ -29,6 +40,8 @@ end
 
 return {
   glaze = glaze,
+  has = has,
+  set = set,
   get = get,
   reset = reset,
 }
