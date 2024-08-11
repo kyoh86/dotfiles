5dbae304be02842a5dda57ad69c6d9235e1a710a
kyoh86 <me@kyoh86.dev>
2024-08-11 19:48:28 +0900

    use ddu-source-local_ddu

---
+++ nvim/lua/kyoh86/plug/ddu/source/local_ddu.lua
@@ -0,0 +1,18 @@
+local helper = require("kyoh86.plug.ddu.helper")
+---@type LazySpec
+local spec = {
+  "kyoh86/ddu-source-local_ddu",
+  dev = true,
+  dependencies = { "ddu.vim" },
+  config = function()
+    helper.setup("local-ddu", {
+      sources = { { name = "local_ddu", options = { defaultAction = "start" } } },
+    }, {
+      start = {
+        key = "<leader><leader>f",
+        desc = "Start any local ddu",
+      },
+    })
+  end,
+}
+return spec
