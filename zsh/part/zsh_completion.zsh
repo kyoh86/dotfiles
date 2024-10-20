73caaf3b48ecc2ac694fe949a0ec10082330649d
kyoh86 <me@kyoh86.dev>
2024-10-20 11:05:46 +0900

    completions

--- zsh/part/zsh_completion.zsh
+++ zsh/part/zsh_completion.zsh
@@ -1,7 +1,12 @@
 # 自動補完の設定
-if [ ! -d "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/completions" ]; then
-    mkdir -p "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/completions"
+cache_completions="${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/completions"
+if [ ! -d "${cache_completions}" ]; then
+    mkdir -p "${cache_completions}"
 fi
-fpath=("${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/completions" $fpath)
+config_completions="${XDG_CACHE_HOME:-${HOME}/.config}/zsh/completions"
+if [ ! -d "${config_completions}" ]; then
+    mkdir -p "${config_completions}"
+fi
+fpath=("${cache_completions}" "${config_completions}" $fpath)
 
 autoload -Uz compinit && compinit
