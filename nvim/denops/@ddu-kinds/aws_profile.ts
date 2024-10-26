f3bd86861e93aa51ecfa38f00f23d7ce9450ba24
GitHub <noreply@github.com>
2024-10-26 14:17:31 +0000

    chore(deps): Update deno dependencies

--- nvim/denops/@ddu-kinds/aws_profile.ts
+++ nvim/denops/@ddu-kinds/aws_profile.ts
@@ -1,6 +1,6 @@
-import { environment } from "jsr:@denops/std@~7.1.0/variable";
-import { ActionFlags, type Actions } from "jsr:@shougo/ddu-vim@~6.1.0/types";
-import { BaseKind } from "jsr:@shougo/ddu-vim@~6.1.0/kind";
+import { environment } from "jsr:@denops/std@~7.3.0/variable";
+import { ActionFlags, type Actions } from "jsr:@shougo/ddu-vim@~6.2.0/types";
+import { BaseKind } from "jsr:@shougo/ddu-vim@~6.2.0/kind";
 import { is, maybe } from "jsr:@core/unknownutil@~4.3.0";
 
 type Params = Record<PropertyKey, never>;
