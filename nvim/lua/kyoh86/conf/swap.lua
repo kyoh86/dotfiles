56517a21276520fb0ec821d29794bb6577caf11d
kyoh86 <me@kyoh86.dev>
2024-11-08 12:52:18 +0900

    Show swap files

--- nvim/lua/kyoh86/conf/swap.lua
+++ nvim/lua/kyoh86/conf/swap.lua
@@ -1,12 +1,43 @@
-vim.api.nvim_create_user_command("SwapClean", function()
-  local dirs = vim.opt.directory:get()
-  for _, dir in pairs(dirs) do
-    for name in vim.iter(vim.fs.dir(dir, {depth= 1})):filter(function(_, type)
-      return type == "file"
-    end) do
-      local file = vim.fs.joinpath(dir, name)
-      vim.print("deleting " .. file)
-      vim.fs.rm(file)
+local function get_swap_files()
+  local dirs = vim.iter(vim.opt.directory:get())
+  local files
+  local dir
+  return function()
+    if not files then
+      if not dir then
+        dir = dirs:next()
+        if not dir then
+          return
+        end
+      end
+      files = vim
+        .iter(vim.fs.dir(dir, { depth = 1 }))
+        :filter(function(_, type)
+          return type == "file"
+        end)
+        :map(function(name)
+          return vim.fs.joinpath(dir, name)
+        end)
+      if not files then
+        return
+      end
     end
+    local n = files:next()
+    if n then
+      return n
+    end
+  end
+end
+
+vim.api.nvim_create_user_command("SwapClean", function()
+  for _, file in vim.iter(get_swap_files()) do
+    vim.print("deleting " .. file)
+    vim.fs.rm(file)
+  end
+end, {})
+
+vim.api.nvim_create_user_command("SwapFiles", function()
+  for file in vim.iter(get_swap_files()) do
+    vim.print(file)
   end
 end, {})
