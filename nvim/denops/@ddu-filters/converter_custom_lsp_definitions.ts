1ec0edc4f35d024b1dbb1d417042e7acdc3acdb7
GitHub <noreply@github.com>
2024-12-14 04:30:26 +0000

    chore(deps): Update deno dependencies

--- nvim/denops/@ddu-filters/converter_custom_lsp_definitions.ts
+++ nvim/denops/@ddu-filters/converter_custom_lsp_definitions.ts
@@ -1,6 +1,6 @@
-import type { FilterArguments } from "jsr:@shougo/ddu-vim@~9.0.0/filter";
-import type { DduItem, Item } from "jsr:@shougo/ddu-vim@~9.0.0/types";
-import { BaseFilter } from "jsr:@shougo/ddu-vim@~9.0.0/filter";
+import type { FilterArguments } from "jsr:@shougo/ddu-vim@~9.1.0/filter";
+import type { DduItem, Item } from "jsr:@shougo/ddu-vim@~9.1.0/types";
+import { BaseFilter } from "jsr:@shougo/ddu-vim@~9.1.0/filter";
 
 type Params = Record<PropertyKey, never>;
 
