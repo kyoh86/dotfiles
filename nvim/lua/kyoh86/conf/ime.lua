5edfe73467784026427cb59a2836946fd7fad52d
kyoh86 <me@kyoh86.dev>
2024-12-14 21:11:05 +0900

    put IME off when InsertLeave

--- nvim/lua/kyoh86/conf/ime.lua
+++ nvim/lua/kyoh86/conf/ime.lua
@@ -1,27 +1,27 @@
 --- Insertモードから抜けるときにIMEを自動でオフにする
-vim.uv
-  .new_async(vim.schedule_wrap(function()
-    local uname = vim.uv.os_uname()
-    local group = vim.api.nvim_create_augroup("kyoh86-conf-ime", {})
-    if uname.sysname == "Linux" then
-      if os.getenv("WSL_DISTRO_NAME") ~= "" then
-        -- TODO
-      elseif vim.fn.executable("ibus") == 1 then
-        vim.api.nvim_create_autocmd("InsertLeave", {
-          group = group,
-          command = "silent! !ibus engine 'xkb:us::eng'",
-        })
-      elseif vim.fn.executable("fcitx-remote") == 1 then
-        vim.api.nvim_create_autocmd("InsertLeave", {
-          group = group,
-          command = "silent! !fcitx-remote -c",
-        })
-      end
-    elseif uname.sysname == "Mac" then
-      vim.api.nvim_create_autocmd("InsertLeave", {
-        group = group,
-        command = [[silent! !osascript -e 'tell application "System Events"' -e 'key code 102' -e 'end tell']],
-      })
-    end
-  end))
-  :send()
+local uname = vim.uv.os_uname()
+local group = vim.api.nvim_create_augroup("kyoh86-conf-ime", {})
+if uname.sysname == "Linux" then
+  if os.getenv("WSL_DISTRO_NAME") ~= "" then
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      group = group,
+      command = "silent! !zenhan.exe 0",
+    })
+  elseif vim.fn.executable("ibus") == 1 then
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      group = group,
+      command = "silent! !ibus engine 'xkb:us::eng'",
+    })
+  elseif vim.fn.executable("fcitx-remote") == 1 then
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      group = group,
+      command = "silent! !fcitx-remote -c",
+    })
+  end
+elseif uname.sysname == "Mac" then
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    group = group,
+    command = [[silent! !osascript -e 'tell application "System Events"' -e 'key code 102' -e 'end tell']],
+  })
+end
+
