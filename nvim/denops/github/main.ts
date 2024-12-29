182572e40736c6905c1ea7973814eb60a5bf504b
kyoh86 <me@kyoh86.dev>
2024-12-29 16:04:29 +0900

    update denops-router

--- nvim/denops/github/main.ts
+++ nvim/denops/github/main.ts
@@ -1,5 +1,5 @@
 import type { Entrypoint } from "jsr:@denops/std@~7.4.0";
-import { Router } from "jsr:@kyoh86/denops-router@0.3.6";
+import { Router } from "jsr:@kyoh86/denops-router@0.3.7";
 import { login } from "./handler/login.ts";
 import {
   issueViewBrowse,
