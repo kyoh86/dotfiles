2f664fde94e8425f98db73378d89b9c6d688e356
kyoh86 <me@kyoh86.dev>
2025-01-01 03:03:45 +0900

    update denops dependencies

--- nvim/denops/github/handler/issue-viewer.ts
+++ nvim/denops/github/handler/issue-viewer.ts
@@ -3,7 +3,7 @@ import type {
   Buffer,
   LoadContext,
   Router,
-} from "jsr:@kyoh86/denops-router@0.4.0";
+} from "jsr:@kyoh86/denops-router@0.4.2";
 import * as buffer from "jsr:@denops/std@~7.4.0/buffer";
 import * as option from "jsr:@denops/std@~7.4.0/option";
 import * as autocmd from "jsr:@denops/std@~7.4.0/autocmd";
