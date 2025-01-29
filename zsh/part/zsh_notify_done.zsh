8bc849192a55f1a4bbbb5a3fb2c443665a671f3f
kyoh86 <me@kyoh86.dev>
2025-01-29 22:09:48 +0900

    unwrap base64

--- zsh/part/zsh_notify_done.zsh
+++ zsh/part/zsh_notify_done.zsh
@@ -5,7 +5,7 @@
 # autocmdを受けて処理の終了を通知してる: ../../nvim/lua/kyoh86/conf/zsh-result.lua
 if [ -n "${NVIM_SERVER_NAME}" ] ; then
   function _notify_precmd_to_nvim() {
-    nvim --server ${NVIM_SERVER_NAME} --headless --remote-send "<cmd>doautocmd User Kyoh86TermNotifReceived:precmd:$?:$KYOH86_VOLATERM_BUFNR:$(echo $history[$(($HISTCMD-1))] | base64)<cr>"
+    nvim --server ${NVIM_SERVER_NAME} --headless --remote-send "<cmd>doautocmd User Kyoh86TermNotifReceived:precmd:$?:$KYOH86_VOLATERM_BUFNR:$(echo $history[$(($HISTCMD-1))] | base64 -w 0)<cr>"
   }
   add-zsh-hook precmd _notify_precmd_to_nvim
 fi
