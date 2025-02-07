8d2d3d1650c1efa2bb8c06820f4dd6f8a4017af6
kyoh86 <me@kyoh86.dev>
2025-02-08 01:24:45 +0900

    fix slow source

--- nvim/denops/@ddu-sources/slow.ts
+++ nvim/denops/@ddu-sources/slow.ts
@@ -1,14 +1,8 @@
 import type {} from "jsr:@denops/std@~7.4.0";
 import type { GatherArguments } from "jsr:@shougo/ddu-vim@~9.5.0/source";
-import {
-  ActionFlags,
-  type Actions,
-  type Item,
-} from "jsr:@shougo/ddu-vim@~9.5.0/types";
+import type { Item } from "jsr:@shougo/ddu-vim@~9.5.0/types";
 import { BaseSource } from "jsr:@shougo/ddu-vim@~9.5.0/source";
 import type { ActionData } from "jsr:@shougo/ddu-kind-word@~0.4.1";
-import { environment } from "jsr:@denops/std@~7.4.0/variable";
-import { is, maybe } from "jsr:@core/unknownutil@~4.3.0";
 
 type Params = Record<PropertyKey, never>;
 
@@ -45,27 +39,6 @@ export class Source extends BaseSource<Params, ActionData> {
       },
     });
   }
-
-  override actions: Actions<Params> = {
-    setenv: async (args) => {
-      if (args.items.length != 1) {
-        console.error(`multiple items are not supported to call "setenv"`);
-        return ActionFlags.None;
-      }
-      const action = maybe(
-        args.items[0].action,
-        is.ObjectOf({
-          name: is.String,
-        }),
-      );
-      if (!action || !action.name) {
-        console.error("invalid selected item (having no name)");
-        return ActionFlags.None;
-      }
-      await environment.set(args.denops, "AWS_PROFILE", action.name);
-      return ActionFlags.None;
-    },
-  };
 
   override params(): Params {
     return {};
