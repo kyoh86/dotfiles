240af9db7f896873facd86aac3e5893775bb4b48
kyoh86 <me@kyoh86.dev>
2025-03-02 09:48:39 +0900

    update dependencies

--- nvim/denops/kyoh86/plug/ddc.ts
+++ nvim/denops/kyoh86/plug/ddc.ts
@@ -1,6 +1,6 @@
-import type { UserSource } from "jsr:@shougo/ddc-vim@~9.3.0/types";
-import { BaseConfig } from "jsr:@shougo/ddc-vim@~9.3.0/config";
-import type { ConfigArguments } from "jsr:@shougo/ddc-vim@~9.3.0/config";
+import type { UserSource } from "jsr:@shougo/ddc-vim@~9.4.0/types";
+import { BaseConfig } from "jsr:@shougo/ddc-vim@~9.4.0/config";
+import type { ConfigArguments } from "jsr:@shougo/ddc-vim@~9.4.0/config";
 
 export class Config extends BaseConfig {
   override config(args: ConfigArguments): Promise<void> {
