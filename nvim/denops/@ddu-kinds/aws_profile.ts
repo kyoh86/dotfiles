f7abee45fffbbe8a14f3bc6e80a20f21e3b48907
kyoh86 <me@kyoh86.dev>
2025-03-24 12:32:14 +0900

    update dependencies

--- nvim/denops/@ddu-kinds/aws_profile.ts
+++ nvim/denops/@ddu-kinds/aws_profile.ts
@@ -1,6 +1,6 @@
 import { environment } from "jsr:@denops/std@~7.5.0/variable";
-import { ActionFlags, type Actions } from "jsr:@shougo/ddu-vim@~10.1.0/types";
-import { BaseKind } from "jsr:@shougo/ddu-vim@~10.1.0/kind";
+import { ActionFlags, type Actions } from "jsr:@shougo/ddu-vim@~10.3.0/types";
+import { BaseKind } from "jsr:@shougo/ddu-vim@~10.3.0/kind";
 import { is, maybe } from "jsr:@core/unknownutil@~4.3.0";
 
 type Params = Record<PropertyKey, never>;
