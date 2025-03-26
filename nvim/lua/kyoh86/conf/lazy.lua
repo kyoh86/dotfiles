9fff6289373c77bfdee9f159825750daaba88f09
kyoh86 <me@kyoh86.dev>
2025-03-26 14:11:20 +0900

    support forking on lazy.nvim

--- nvim/lua/kyoh86/conf/lazy.lua
+++ nvim/lua/kyoh86/conf/lazy.lua
@@ -1,4 +1,23 @@
+local dev_path_origin = vim.env.HOME .. "/Projects/github.com"
+local default_dev_path = dev_path_origin .. "/kyoh86"
+local url_pattern = "^https://github%.com/([^/]+)/([^/]+)(%.git)$"
+
+--- Get the path of the plugin in development.
+--- @param plugin LazyPlugin
+--- @return string
+local function dev_path(plugin)
+  local url = plugin.url
+  if url == nil then
+    return default_dev_path
+  end
+  local owner, name = string.match(url, url_pattern)
+  if owner == nil or name == nil then
+    return default_dev_path
+  end
+  vim.print(dev_path_origin .. "/" .. owner .. "/" .. name)
+  return dev_path_origin .. "/" .. owner .. "/" .. name
+end
 local M = {
-  dev_path = "~/Projects/github.com/kyoh86",
+  dev_path = dev_path,
 }
 return M
