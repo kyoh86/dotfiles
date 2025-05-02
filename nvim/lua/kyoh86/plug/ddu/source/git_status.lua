0ac05eaa9cd1eeeaca4580bb428e1cb3421cbc13
kyoh86 <me@kyoh86.dev>
2025-05-03 08:49:55 +0900

    never opens deleted file from ddu-source-git_status

--- nvim/lua/kyoh86/plug/ddu/source/git_status.lua
+++ nvim/lua/kyoh86/plug/ddu/source/git_status.lua
@@ -1,27 +1,57 @@
 local helper = require("kyoh86.plug.ddu.helper")
 
+local deletedTree = vim.regex("D $")
+local function isDeleted(status)
+  return status == "D  " or deletedTree:match_str(status)
+end
+
+local function custom_open(editor)
+  return function(args)
+    for _, item in ipairs(args.items) do
+      if item.action == nil then
+      -- noop
+      else
+        local action = item.action
+        if isDeleted(action.status) then
+          -- noop
+        else
+          vim.cmd(string.format("%s %s", editor, action.path))
+        end
+      end
+    end
+    return 0
+  end
+end
+
 ---@type LazySpec
 local spec = {
   "kuuote/ddu-source-git_status",
   dependencies = { "ddu.vim" },
-  config = helper.setup_func("git-status", {
-    sources = { { name = "git_status" } },
-    kindOptions = {
-      git_status = {
-        defaultAction = "open",
+  config = function()
+    vim.fn["ddu#custom#action"]("kind", "git_status", "custom:edit", custom_open("edit"))
+    vim.fn["ddu#custom#action"]("kind", "git_status", "custom:vnew", custom_open("vnew"))
+    vim.fn["ddu#custom#action"]("kind", "git_status", "custom:new", custom_open("new"))
+    helper.setup("git-status", {
+      sources = { {
+        name = "git_status",
+      } },
+      kindOptions = {
+        git_status = {
+          defaultAction = "custom:edit",
+        },
       },
-    },
-  }, {
-    start = {
-      key = "<leader>fgs",
-      desc = "Git Status",
-    },
-    localmap = {
-      ["<leader>a"] = { action = "itemAction", params = { name = "add" } },
-      ["<leader>r"] = { action = "itemAction", params = { name = "reset" } },
-      ["<leader>x"] = { action = "itemAction", params = { name = "open", params = { command = "new" } } },
-      ["<leader>v"] = { action = "itemAction", params = { name = "open", params = { command = "vnew" } } },
-    },
-  }),
+    }, {
+      start = {
+        key = "<leader>fgs",
+        desc = "Git Status",
+      },
+      localmap = {
+        ["<leader>a"] = { action = "itemAction", params = { name = "add" } },
+        ["<leader>r"] = { action = "itemAction", params = { name = "reset" } },
+        ["<leader>x"] = { action = "itemAction", params = { name = "custom:new" } },
+        ["<leader>v"] = { action = "itemAction", params = { name = "custom:vnew" } },
+      },
+    })
+  end,
 }
 return spec
