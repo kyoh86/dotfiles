1b7f5692d2b762fca7f32a5cdb1883f7895194bb
kyoh86 <me@kyoh86.dev>
2025-05-24 14:47:21 +0900

    Expand command-alias with also <Tab> not only <Space>

--- nvim/lua/kyoh86/conf/alias.lua
+++ nvim/lua/kyoh86/conf/alias.lua
@@ -1,5 +1,28 @@
 -- To fix typos
+-- The first argument "{mode}"; "ia", "ca" or "!a" for abbreviation in Insert mode, Cmdline mode, or both, respectively
+-- See: "{mode}" in :help |nvim_set_keymap()|
 vim.keymap.set({ "!a" }, "terraofrm", "terraform")
 vim.keymap.set({ "!a" }, "terrafrom", "terraform")
 vim.keymap.set({ "!a" }, "keyamp", "keymap")
-vim.keymap.set({ "!a" }, "cc", "CodeCompanion")
+
+-- CommandモードでTabキーを押したときに略語を展開
+vim.api.nvim_set_keymap("c", "<Tab>", "v:lua.require('kyoh86.conf.alias').expand_cmd_abbrev()", { expr = true })
+
+-- 略語を展開する汎用関数
+local function expand_cmd_abbrev()
+  local cmdline = vim.fn.getcmdline()
+  local expand = vim.fn.maparg(cmdline, "c", true)
+
+  -- 現在の入力が略語として登録されているか確認
+  if expand ~= "" then
+    -- 略語を展開
+    return vim.api.nvim_replace_termcodes("<C-U>" .. expand, true, true, true)
+  else
+    -- 通常のTab動作
+    return vim.api.nvim_replace_termcodes("<Tab>", true, true, true)
+  end
+end
+
+return {
+  expand_cmd_abbrev = expand_cmd_abbrev,
+}
