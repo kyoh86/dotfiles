58c3e81963f30352c139ef83292b5f0e5b84552e
kyoh86 <me@kyoh86.dev>
2025-08-09 16:27:43 +0900

    remove unneccessary codecompanion.nvim

--- nvim/lua/kyoh86/plug/ai.lua
+++ nvim/lua/kyoh86/plug/ai.lua
@@ -1,42 +1,6 @@
 ---@type LazySpec[]
 local specs = {
   {
-    "olimorris/codecompanion.nvim",
-    opts = {
-      extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            make_vars = true, -- Convert resources to #variables
-            show_result_in_chat = true, -- Show mcp tool results in chat
-            make_slash_commands = true, -- Add prompts as /slash commands
-          },
-        },
-      },
-      strategies = {
-        chat = {
-          keymaps = {
-            completion = {
-              modes = {
-                i = "<C-Space>",
-              },
-            },
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("codecompanion").setup(opts)
-      vim.keymap.set({ "ca" }, "ccc", "CodeCompanionChat")
-    end,
-    dependencies = {
-      "plenary.nvim",
-      "nvim-treesitter",
-      "nvim-treesitter-textobjects",
-      "mcphub.nvim",
-    },
-  },
-  {
     "ravitemer/mcphub.nvim",
     build = "bundled_build.lua",
     opts = {
