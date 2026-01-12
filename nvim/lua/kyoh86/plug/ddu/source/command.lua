ee5c00fb40eef5a4898038426cde517ef700f0ec
kyoh86 <me@kyoh86.dev>
2026-01-12 17:50:19 +0900

    use kyoh86/ddu-source-command_history instead of matsui54/ddu-source-command_history

--- nvim/lua/kyoh86/plug/ddu/source/command.lua
+++ nvim/lua/kyoh86/plug/ddu/source/command.lua
@@ -1,28 +1,53 @@
 local helper = require("kyoh86.plug.ddu.helper")
 
 ---@type LazySpec[]
-local spec = { {
-  "kyoh86/ddu-kind-command",
-}, {
-  "kyoh86/ddu-source-command",
-  dependencies = { "ddu.vim", "ddu-kind-word", "ddu-kind-command" },
-  config = helper.setup_func("command", {
-    sources = { { name = "command" } },
-    kindOptions = {
-      command = {
-        defaultAction = "edit",
+local spec = {
+  {
+    "kyoh86/ddu-kind-command",
+  },
+  {
+    "kyoh86/ddu-source-command",
+    dependencies = { "ddu.vim", "ddu-kind-word", "ddu-kind-command" },
+    config = helper.setup_func("command", {
+      sources = { { name = "command" } },
+      kindOptions = {
+        command = {
+          defaultAction = "edit",
+        },
       },
-    },
-    uiParams = {
-      ff = {
-        startAutoAction = true,
+      uiParams = {
+        ff = {
+          startAutoAction = true,
+        },
       },
-    },
-  }, {
-    start = {
-      key = "<leader>f:",
-      desc = "コマンド",
-    },
-  }),
-} }
+    }, {
+      start = {
+        key = "<leader>f:",
+        desc = "コマンド",
+      },
+    }),
+  },
+  {
+    "kyoh86/ddu-source-command_history",
+    dependencies = { "ddu.vim", "ddu-kind-word", "ddu-kind-command" },
+    config = helper.setup_func("command-history", {
+      sources = { { name = "command_history", params = { parse = true } } },
+      kindOptions = {
+        command = {
+          defaultAction = "edit",
+        },
+      },
+      uiParams = {
+        ff = {
+          startAutoAction = true,
+        },
+      },
+    }, {
+      start = { key = "<leader>f;", desc = "コマンド履歴" },
+      localmap = {
+        ["<leader>e"] = { action = "itemAction", params = { name = "edit" } },
+      },
+    }),
+  },
+}
 return spec
