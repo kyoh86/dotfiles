7ec11e1e6e3c2cbde23d7bcac6da2e92e8ffb036
kyoh86 <me@kyoh86.dev>
2026-01-13 14:30:19 +0900

    replace "$HOME" in zsh util

--- zsh/part/gcloud.zsh
+++ zsh/part/gcloud.zsh
@@ -1,5 +1,5 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/kyoh86/gc/google-cloud-sdk/path.zsh.inc' ]; then . '/home/kyoh86/gc/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f "$HOME/gc/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/gc/google-cloud-sdk/path.zsh.inc"; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/home/kyoh86/gc/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/kyoh86/gc/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f "$HOME/gc/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/gc/google-cloud-sdk/completion.zsh.inc"; fi
