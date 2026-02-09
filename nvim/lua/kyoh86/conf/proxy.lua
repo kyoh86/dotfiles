5b5ec931c681319e72184b6f7ebe34053b756756
kyoh86 <me@kyoh86.dev>
2026-02-09 17:35:53 +0900

    fix: search mise via "exepath"

--- nvim/lua/kyoh86/conf/proxy.lua
+++ nvim/lua/kyoh86/conf/proxy.lua
@@ -1,1 +1,5 @@
-vim.g.nvim_proxy_deno_command = { vim.env.HOME .. "/.local/bin/mise", "exec", "deno", "--", "deno" }
+local glaze = require("kyoh86.lib.glaze")
+local mise_path = glaze.ensure("mise-path", function()
+  return vim.fn.exepath("mise")
+end)
+vim.g.nvim_proxy_deno_command = { mise_path, "exec", "deno", "--", "deno" }
