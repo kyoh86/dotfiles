4753dcfb06a5bc614b0341eda7251dd7b7bd5890
kyoh86 <me@kyoh86.dev>
2026-06-09 01:39:12 +0900

    color pane selection children

--- dotfiles-tool/tmux-layout-paredit/types.go
+++ dotfiles-tool/tmux-layout-paredit/types.go
@@ -61,6 +61,5 @@ type State struct {
 const (
 	SelectBG      = "#c2c2f2" // 1番目の子の色 (rgba(194, 194, 242))
 	SecondChildBG = "#dadaf7" // 2番目の子の色 (ベース色 + 半透明白色40%)
-	LeafSelectBG  = "#f2c2c2" // leaf がフォーカスされた場合 (rgba(242, 194, 194))
 	Step          = 5
 )
