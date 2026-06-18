79ef2c2def8a4e6725a8c3fbcfd749127badd0c5
kyoh86 <me@kyoh86.dev>
2026-06-18 13:00:33 +0900

    fix tmux environment setup

--- zsh/part/aws_completion.zsh
+++ zsh/part/aws_completion.zsh
@@ -1,6 +1,5 @@
 aws_completion() {
   if command -v mise >/dev/null 2>&1; then
-    echo "hoge" > ~/.cache/hoge
     dirname=$(dirname "$(mise which aws)")
     complete -C "${dirname}/aws_completer" aws
   fi
--- zsh/part/env.zsh
+++ zsh/part/env.zsh
@@ -10,7 +10,7 @@ export GID
 # XDG Base Directories
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
-export XDG_STAGE_HOME="${XDG_STAGE_HOME:-${HOME}/.local/state}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-${HOME}/.local/state}"
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-${HOME}/.cache}
 
 # Zsh
