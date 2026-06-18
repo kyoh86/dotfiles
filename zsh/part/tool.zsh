99646a810d1a105208a2b22b993d9eaa940e632d
kyoh86 <me@kyoh86.dev>
2026-06-18 13:00:33 +0900

    fix tmux environment setup

--- zsh/part/tool.zsh
+++ zsh/part/tool.zsh
@@ -1,9 +1,5 @@
 # GnuPGでPassphraseを使う際のTTYを設定
-if [ "$NVIM_TERMINAL" = "1" ]; then
-    : # in nvim
-else
-    export GPG_TTY="$(tty)"
-fi
+export GPG_TTY="$(tty)"
 
 # mongoshでのファイル実行: 常に--nodbで始めるオプション
 # mongorun hoge.js のように使う
